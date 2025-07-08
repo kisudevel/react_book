b6a451b06024aa5567d6db294b0d53341e8fffa7
kisudevel <kisudevel@gmail.com>
2025-07-08 17:22:59 +0900

    12.2.8 코드 수정

--- ch12/02/8/src/hooks/useAuthContext.ts
+++ ch12/02/8/src/hooks/useAuthContext.ts
@@ -1,12 +1,15 @@
+/* eslint-disable no-constant-condition */
 import { use } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 export function useAuthContext() {
-  const context = use(AuthContext);
-  if (!context) {
-    throw new Error(
-      "useAuthContext AuthProvider로 감싼 컴포넌트 안에서만 호출할 수 있습니다."
-    );
+  if (true) {
+    const context = use(AuthContext);
+    if (!context) {
+      throw new Error(
+        "useAuthContext AuthProvider로 감싼 컴포넌트 안에서만 호출할 수 있습니다."
+      );
+    }
+    return context;
   }
-  return context;
 }
